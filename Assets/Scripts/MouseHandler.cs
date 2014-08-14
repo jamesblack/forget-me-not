649e0da7f74f577f4121c70350720f7eae93a438
James Black <james@james-black.com>
2014-08-13 21:12:27 -0600

    Initial Art/Prefabs, also adds selection scripting

---
+++ Assets/Scripts/MouseHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseHandler : MonoBehaviour
+{
+
+    public Survivor Selected;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        Debug.Log("Click Detected");
+
+	        RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.zero);
+
+            if (hitInfo && hitInfo.transform.gameObject.GetComponent<Entity>())
+	        {
+	            Entity target = hitInfo.transform.gameObject.GetComponent<Entity>();
+
+	            if (target is Survivor)
+	            {
+	                Survivor survivor = target as Survivor;
+                    if (Selected == survivor)
+                    {
+                        Debug.Log("You Already Selected this Fool");
+                        return;
+                    }
+
+                    Debug.Log("You selected " + survivor.FullName);
+                    Selected = survivor;
+                }
+                else if (target is Zombie)
+                {
+                    Debug.Log("Stop Clicking Zombies you fool!");    
+                }
+	        }
+	        else
+	        {
+	            Selected = null;
+	        }
+	    }
+	}
+}
---
+++ Assets/Scripts/Survivor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Survivor : Entity
+{
+
+    public string FirstName = "John";
+    public string LastName = "Doe";
+
+    public string FullName { get { return FirstName + " " +  LastName; } }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
